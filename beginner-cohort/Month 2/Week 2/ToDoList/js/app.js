e3c6de1abf3062dd4421157c5e81fabd6ff717ff
Chirag Chhajed <chhajedchirag18@gmail.com>
2022-08-28 15:53:13 +0530

    Changes made

--- beginner-cohort/Month 2/Week 2/ToDoList/js/app.js
+++ beginner-cohort/Month 2/Week 2/ToDoList/js/app.js
@@ -7,6 +7,7 @@ const form = document.querySelector("form");
 const inputField = document.getElementById("task");
 const submitBtn = document.querySelector(".submit-btn");
 const todoList = document.querySelector(".list");
+const alert = document.querySelector(".alert");
 
 // Edit Option
 let editElement;
@@ -17,17 +18,29 @@ let editID = "";
 form.addEventListener("submit", addItem);
 
 // Functions
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+
+  setTimeout(function () {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
 function addItem(e) {
   e.preventDefault();
+  // Creating Elements
   const listitem = document.createElement("li");
-  const paragraph = document.createElement("p")
-  paragraph.classList.add("para")
+  const paragraph = document.createElement("p");
+  // Adding classes
+  paragraph.classList.add("para");
   listitem.classList.add("list-item");
+
   paragraph.innerText = inputField.value;
   todoList.appendChild(listitem);
   inputField.value = "";
   const btnContainer = document.createElement("div");
-  btnContainer.classList.add("btns")
+  btnContainer.classList.add("btns");
   // Check button
   const checkBtn = document.createElement("button");
   checkBtn.classList.add("checkBtn");
@@ -37,17 +50,25 @@ function addItem(e) {
   deleteBtn.classList.add("deleteBtn");
   deleteBtn.innerHTML = `<i class="fas fa-trash"></i>`;
 
-  listitem.appendChild(paragraph)
+  // Appending element
+  listitem.appendChild(paragraph);
   listitem.appendChild(btnContainer);
   btnContainer.appendChild(deleteBtn);
   btnContainer.appendChild(checkBtn);
+
+  displayAlert("Task added","added")
+
+  // Event Listeners for Buttons
   checkBtn.addEventListener("click", function () {
     listitem.style.textDecoration = "line-through";
+    displayAlert("Task Done","check")
   });
-  checkBtn.addEventListener("dblclick",function(){
+  checkBtn.addEventListener("dblclick", function () {
     listitem.style.textDecoration = "none";
-  })
+    displayAlert("Task Undone","uncheck")
+  });
   deleteBtn.addEventListener("click", function () {
     todoList.removeChild(listitem);
+    displayAlert("Task Deleted","delete")
   });
 }
