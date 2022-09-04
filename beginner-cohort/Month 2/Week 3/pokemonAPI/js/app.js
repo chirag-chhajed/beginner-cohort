653c40acd2d3a5f7f976c9edd42eb0fff45a887e
Chirag Chhajed <chhajedchirag18@gmail.com>
2022-09-04 14:52:37 +0530

    js added

--- beginner-cohort/Month 2/Week 3/pokemonAPI/js/app.js
+++ beginner-cohort/Month 2/Week 3/pokemonAPI/js/app.js
@@ -1,51 +1,59 @@
+// Selecting elements
 const pokedex = document.querySelector(".pokedex");
 const searchBar = document.querySelector(".search");
 const inputField = document.getElementById("pokemon");
 const search = document.querySelector("#search");
 const screen = document.querySelector(".screen");
 const names = document.querySelector(".name");
-const screenTwo = document.querySelector(".screen-two")
-const type = document.querySelector(".type-container")
+const screenTwo = document.querySelector(".screen-two");
+const type = document.querySelector(".type-container");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
 
-
+// Audio
+const successSound = new Audio(
+  "../assets/sound/Success sound effect (copyright free).mp3"
+);
+const errorSound = new Audio("../assets/sound/Error - Sound Effect (HD).mp3");
+const pokemonSong = new Audio("../assets/New folder/Theme.mp3");
 search.addEventListener("submit", getPokemon);
 function capitalizeFirstLetter(string) {
   return string.toUpperCase();
 }
 
+playButton.addEventListener("click",pokemonSong.play());
+pauseButton.addEventListener("click",pokemonSong.pause())
+
 function lowerCaseName(string) {
   return string.toLowerCase();
 }
 
-function getPokemon(e){
-    
-    const pokemonName = inputField.value;
+function getPokemon(e) {
+  const pokemonName = inputField.value;
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${lowerCaseName(pokemonName)}`)
+  fetch(`https://pokeapi.co/api/v2/pokemon/${lowerCaseName(pokemonName)}`)
     .then((response) => response.json())
-    .then((data)=>{
-       screen.style.backgroundImage = `url(${data.sprites.other["official-artwork"].front_default})`;
-        screen.style.backgroundSize = "contain"
-        screen.style.backgroundRepeat = "no-repeat"
-        screen.style.backgroundPosition = "center"
-        names.innerHTML = `<p>${capitalizeFirstLetter(pokemonName)}</p>`;
-        screenTwo.innerHTML = `<p>Weight: ${data.weight}</p><p>Height: ${data.height}</p>`;
-        type.innerHTML = `<p>${capitalizeFirstLetter(data.types[0].type.name)}</p>`;
-
-      })
-      .catch((err) => {
-        screen.style.backgroundImage = `url(../assets/cross.png)`;
-        screen.style.backgroundSize = "contain"
-        screen.style.backgroundRepeat = "no-repeat"
-        screen.style.backgroundPosition = "center"
-        names.innerHTML = `<p>NOT A POKEMON</p>`;
-        screenTwo.innerHTML = `<p>😑😑😑</p>`;
-        type.innerHTML = `<p>NONE</p>`
-      })
-    e.preventDefault();
+    .then((data) => {
+      screen.style.backgroundImage = `url(${data.sprites.other["official-artwork"].front_default})`;
+      screen.style.backgroundSize = "contain";
+      screen.style.backgroundRepeat = "no-repeat";
+      screen.style.backgroundPosition = "center";
+      names.innerHTML = `<p>${capitalizeFirstLetter(pokemonName)}</p>`;
+      screenTwo.innerHTML = `<p>Weight: ${data.weight}</p><p>Height: ${data.height}</p>`;
+      type.innerHTML = `<p>${capitalizeFirstLetter(
+        data.types[0].type.name
+      )}</p>`;
+      successSound.play();
+    })
+    .catch((err) => {
+      screen.style.backgroundImage = `url(../assets/cross.png)`;
+      screen.style.backgroundSize = "contain";
+      screen.style.backgroundRepeat = "no-repeat";
+      screen.style.backgroundPosition = "center";
+      names.innerHTML = `<p>NOT A POKEMON</p>`;
+      screenTwo.innerHTML = `<p>😑😑😑</p>`;
+      type.innerHTML = `<p>NONE</p>`;
+      errorSound.play();
+    });
+  e.preventDefault();
 }
-{/* <div class="pokemonInfos">
-          <h1>${capitalizeFirstLetter(data.name)}</h3>
-          <p>Weight: ${data.weight}</p>
-          <p>Height: ${data.height}</p>
-        </div> */}
