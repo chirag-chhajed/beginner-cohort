33bfb7811d10f1d5548841810840e41dcceb62a8
Chirag Chhajed <chhajedchirag18@gmail.com>
2022-12-04 19:40:05 +0530

    frontend

--- beginner-cohort/Month 5/Week 1/index.js
+++ beginner-cohort/Month 5/Week 1/index.js
@@ -1,10 +1,25 @@
 import express from "express"
+import path,{dirname} from "path"
+import { fileURLToPath } from 'url';
 import students from "./routes/students.js"
+
 const app = express()
 const port = 5000
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+console.log(__dirname)
 
+
+// app.use(express.urlencoded({ extended: false }));
+
+app.use('/', express.static(path.join(__dirname,"/public")));
 
 app.use(express.json())
+
+app.use('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,"/public","/index.html"))
+})
+console.log(dirname('./'))
 
 app.use('/api/v1/students',students)
 
